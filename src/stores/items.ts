622be51e14fe63f353205669d175afcbbb3570ae
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-05-08 11:02:28 +0200

    Use Firebase as a storage instead of localStorage

--- src/stores/items.ts
+++ src/stores/items.ts
@@ -1,24 +1,47 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
 import { writable, derived, Writable, Readable } from 'svelte/store';
 import type Item from '../types/item';
 
 type ItemList = Record<string, Item>;
 type ItemListArray = Array<Item>;
 
-let storedItems: ItemList = {};
+// Setup Firebase
+const firebaseConfig = {
+	apiKey: 'AIzaSyApxLOuQ3nldNpk4ZRe_uvoaV_86JOVx98',
+	authDomain: 'shopdrop-51322.firebaseapp.com',
+	databaseURL: 'https://shopdrop-51322-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'shopdrop-51322',
+	storageBucket: 'shopdrop-51322.appspot.com',
+	messagingSenderId: '543806112650',
+	appId: '1:543806112650:web:d9bd7db712f1da07c8fe73'
+};
 
-if (typeof window !== 'undefined') {
-	const storedItemsFromStorage = window.localStorage.getItem('items');
-	storedItems = storedItemsFromStorage ? JSON.parse(storedItemsFromStorage) : storedItems;
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig);
 }
 
-export const items: Writable<ItemList> = writable(storedItems);
+// Create the empty store
+export const items: Writable<ItemList> = writable({});
 
-items.subscribe((value) => {
-	if (typeof window !== 'undefined') {
-		window.localStorage.setItem('items', JSON.stringify(value));
-	}
+// Fetch data from Firebase and listen for updates
+const storedItemsTable = firebase.database().ref('list');
+
+storedItemsTable.on('value', (snapshot) => {
+	items.set(snapshot.val());
 });
 
+// Fetch changes to store and save it to Firebase
+items.subscribe((value) => {
+	if (Object.keys(value).length <= 0) {
+		// Do not save empty set, from startup
+		return;
+	}
+
+	firebase.database().ref('list').set(value);
+});
+
+// Create a dirived store that is an array instead of an object, easier to loop through
 export const itemsArray: Readable<ItemListArray> = derived(items, ($items) =>
 	Object.values($items)
 );
