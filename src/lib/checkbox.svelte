84bf1a3c2b14f517961c2354c261af760c117d4a
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-05-10 21:52:23 +0200

    Fix checkbox design

--- src/lib/checkbox.svelte
+++ src/lib/checkbox.svelte
@@ -2,18 +2,33 @@
 	export let id: string = '';
 	export let name: string = '';
 	export let value: string = '';
-	export let checked: boolean;
+	export let checked: boolean = false;
 	export let hidden: boolean = false;
+
+	import IconCheckboxEmpty from '$lib/icons/checkbox-empty.svelte';
+	import IconCheckboxChecked from '$lib/icons/checkbox-checked.svelte';
 </script>
 
-<input type="checkbox" {id} {name} {checked} {value} class:hidden on:change {...$$restProps} />
+<input type="checkbox" {id} {name} {checked} {value} on:change {...$$restProps} />
+
+{#if !hidden && checked}
+	<label for={id} class="icon">
+		<IconCheckboxChecked on:click={() => (checked = !checked)} />
+	</label>
+{/if}
+
+{#if !hidden && !checked}
+	<label for={id} class="icon">
+		<IconCheckboxEmpty on:click={() => (checked = !checked)} />
+	</label>
+{/if}
 
 <style>
-	input {
-		border: 1px #999 solid;
+	input[type='checkbox'] {
+		display: none;
 	}
 
-	.hidden {
-		display: none;
+	.icon {
+		margin-right: 0.75em;
 	}
 </style>
