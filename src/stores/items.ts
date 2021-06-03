13bb17d17c070b645dcbc86f11636be33c4247c4
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-06-03 08:16:31 +0200

    Use firebase realtime production database

--- src/stores/items.ts
+++ src/stores/items.ts
@@ -1,27 +1,17 @@
 import { writable, derived, Writable, Readable } from 'svelte/store';
 import type Item from '../types/item';
 import { settings } from './settings';
+import firebaseConfig from '../../firebase.config.json';
 
 type ItemList = Record<string, Item>;
 type ItemListArray = Array<Item>;
-
-// Setup Firebase
-const firebaseConfig = {
-	apiKey: 'AIzaSyApxLOuQ3nldNpk4ZRe_uvoaV_86JOVx98',
-	authDomain: 'shopdrop-51322.firebaseapp.com',
-	databaseURL: 'https://shopdrop-51322-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'shopdrop-51322',
-	storageBucket: 'shopdrop-51322.appspot.com',
-	messagingSenderId: '543806112650',
-	appId: '1:543806112650:web:d9bd7db712f1da07c8fe73'
-};
 
 let listId = null;
 
 // Create the empty store
 export const items: Writable<ItemList> = writable({});
 
-Promise.all([import('firebase/app'), import('firebase/database')])
+Promise.all([import('firebase/app'), import('firebase/database'), import('firebase/auth')])
 	.then((x) => x[0].default)
 	.then((firebase) => {
 		if (!firebase.apps.length) {
@@ -49,17 +39,25 @@ Promise.all([import('firebase/app'), import('firebase/database')])
 			};
 		});
 
-		// Fetch changes to store and save it to Firebase
-		items.subscribe((value) => {
-			if (value && Object.keys(value).length <= 0) {
-				// Do not save empty set, from startup
-				return;
-			}
+		firebase
+			.auth()
+			.signInAnonymously()
+			.then(() => {
+				// Fetch changes to store and save it to Firebase
+				items.subscribe((value) => {
+					if (value && Object.keys(value).length <= 0) {
+						// Do not save empty set, from startup
+						return;
+					}
 
-			if (listId) {
-				firebase.database().ref(listId).set(value);
-			}
-		});
+					if (listId) {
+						firebase.database().ref(listId).set(value);
+					}
+				});
+			})
+			.catch((err) => {
+				console.log('Could not authorize against the database.', err);
+			});
 	});
 
 // Create a dirived store that is an array instead of an object, easier to loop through
