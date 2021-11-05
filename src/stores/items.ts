b91ab4395543a6a64d3e8b76e9152731cb832818
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-11-05 16:38:27 +0100

    Use env vars instead of conf file

--- src/stores/items.ts
+++ src/stores/items.ts
@@ -1,12 +1,21 @@
 import { writable, derived, Writable, Readable } from 'svelte/store';
 import type Item from '../types/item';
 import { settings } from './settings';
-import firebaseConfig from '../../firebase.config.json';
 
 type ItemList = Record<string, Item>;
 type ItemListArray = Array<Item>;
 
 let listId = null;
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 // Create the empty store
 export const items: Writable<ItemList> = writable({});
